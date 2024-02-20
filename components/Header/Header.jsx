e627f7fe1139c0261e2f3f499dcadf0b15c373b4
AdrienW86 <adrien_weiss@outlook.fr>
2024-02-20 17:19:53 +0100

    mise en lign

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -29,15 +29,26 @@ const handleMenuToggle = () => {
 
 useEffect(() => {
   const handleScroll = () => {
-  const currentScrollPos = window.pageYOffset;
-      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 150);
-      setPrevScrollPos(currentScrollPos);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [prevScrollPos]);
+    if (isMenuOpen) {
+      console.log('empeche')
+      document.body.style.overflow = 'hidden';
+    } else {
+      // Activer le défilement si le menu est fermé
+      document.body.style.overflow = 'visible';
+    }
+  };
+
+  // Ajouter l'écouteur d'événement lorsqu'un changement est détecté dans isMenuOpen
+  window.addEventListener('scroll', handleScroll);
+
+  // Nettoyer l'écouteur d'événement lorsque le composant est démonté
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+    // Rétablir la valeur de l'overflow lorsque le composant est démonté
+    document.body.style.overflow = 'visible';
+  };
+}, [isMenuOpen]);
+
 
   useEffect(() => {
     if (router.pathname === '/success') {
@@ -79,8 +90,8 @@ useEffect(() => {
   }, [isMenuOpen]);
   
   return (
-    <header className={styles.header}>
-      <section className={`${styles.test} ${visible ? styles.visible : styles.hidden} `}>
+  <>
+    <header className={`${styles.header} ${visible ? styles.visible : styles.hidden} `}>
       <section className={styles.delivery}>
         <div className={styles.deliveryContent}>
           <div className={styles.deliveryImg}>
@@ -125,118 +136,120 @@ useEffect(() => {
           </div>         
         </div>
       </section>
-      <section className={styles.hamburger_container}>
+    </header>
+    <section className={`${styles.menuBox} ${isMenuOpen ? styles.menuBoxOpen : ''}`}>
+    {isMenuOpen && (
        
-        </section>
-      {isMenuOpen && (
-        <ul className={styles.menu}>
-          <li className={styles.menu_link}>
-            <div>
-              <Link href='/mens' className={styles.link} onClick={handleMenuToggle}> 
-                <p className={styles.linkText}> HOMMES </p> 
-                <Image 
-                  src={Men}
-                  height={70}
-                  width={50}
-                  className={styles.linkImg}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li> 
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/womens' className={styles.link} onClick={handleMenuToggle}> 
-                <p className={styles.linkText}> FEMMES </p> 
-                <Image 
-                  src={Women}
-                  height={70}
-                  width={70}
-                  className={styles.linkImg}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li> 
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/kids' className={styles.link} onClick={handleMenuToggle}> 
-                <p className={styles.linkText}> JUNIORS </p> 
-                <Image 
-                  src={Child}
-                  height={70}
-                  width={60}
-                  className={styles.linkImg}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li> 
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/maillots' className={styles.link} onClick={handleMenuToggle}>
-                <p className={styles.linkText}> MAILLOTS </p> 
-                <Image 
-                  src={Maillot}
-                  height={70}
-                  width={45}
-                  className={styles.linkImg2}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li> 
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/sockets' className={styles.link} onClick={handleMenuToggle}> 
-                <p className={styles.linkText}> CHAUSSETTES </p> 
-                <Image 
-                  src={Socket}
-                  height={60}
-                  width={40}
-                  className={styles.linkImg1}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li> 
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/balms' className={styles.link} onClick={handleMenuToggle}>
-                <p className={styles.linkText}> BAUMES </p> 
-                <Image 
-                  src={Balm}
-                  height={60}
-                  width={50}
-                  className={styles.linkImg2}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li>
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/boxs' className={styles.link} onClick={handleMenuToggle}>
-                <p className={styles.linkText}> COFFRETS </p> 
-                <Image 
-                  src={Box}
-                  height={70}
-                  width={60}
-                  className={styles.linkImg2}
-                  alt="image du panier"
-                />   
-              </Link>
-            </div>
-          </li> 
-          <li className={styles.menu_link}>
-            <div>
-            <Link href='/good-deal' className={styles.link} onClick={handleMenuToggle}>
-                <p className={styles.linkText}> BON PLANS </p> 
-              </Link>
-            </div>
-          </li> 
-        </ul>
-      )}
+       <ul className={styles.menu}>
+       <li className={styles.menu_link}>
+         <div>
+           <Link href='/mens' className={styles.link} onClick={handleMenuToggle}> 
+             <p className={styles.linkText}> HOMMES </p> 
+             <Image 
+               src={Men}
+               height={70}
+               width={50}
+               className={styles.linkImg}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li> 
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/womens' className={styles.link} onClick={handleMenuToggle}> 
+             <p className={styles.linkText}> FEMMES </p> 
+             <Image 
+               src={Women}
+               height={70}
+               width={70}
+               className={styles.linkImg}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li> 
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/kids' className={styles.link} onClick={handleMenuToggle}> 
+             <p className={styles.linkText}> JUNIORS </p> 
+             <Image 
+               src={Child}
+               height={70}
+               width={60}
+               className={styles.linkImg}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li> 
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/maillots' className={styles.link} onClick={handleMenuToggle}>
+             <p className={styles.linkText}> MAILLOTS </p> 
+             <Image 
+               src={Maillot}
+               height={70}
+               width={45}
+               className={styles.linkImg2}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li> 
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/sockets' className={styles.link} onClick={handleMenuToggle}> 
+             <p className={styles.linkText}> CHAUSSETTES </p> 
+             <Image 
+               src={Socket}
+               height={60}
+               width={40}
+               className={styles.linkImg1}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li> 
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/balms' className={styles.link} onClick={handleMenuToggle}>
+             <p className={styles.linkText}> BAUMES </p> 
+             <Image 
+               src={Balm}
+               height={60}
+               width={50}
+               className={styles.linkImg2}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li>
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/boxs' className={styles.link} onClick={handleMenuToggle}>
+             <p className={styles.linkText}> COFFRETS </p> 
+             <Image 
+               src={Box}
+               height={70}
+               width={60}
+               className={styles.linkImg2}
+               alt="image du panier"
+             />   
+           </Link>
+         </div>
+       </li> 
+       <li className={styles.menu_link}>
+         <div>
+         <Link href='/good-deal' className={styles.link} onClick={handleMenuToggle}>
+             <p className={styles.linkText}> BON PLANS </p> 
+           </Link>
+         </div>
+       </li> 
+     </ul>
+    
+     )}
+     <section/>
       <Link  href='/card'> 
         <li className={styles.menu_basket}>
           <Image 
@@ -252,6 +265,6 @@ useEffect(() => {
         </li>      
       </Link>
     </section>
-    </header>
+  </>
   );
 }
